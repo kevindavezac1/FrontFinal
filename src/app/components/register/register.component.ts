111c7b43c9b7651cdcc352dc3ef691f4209e293c
Kevin Davezac <kevindavezac@outlook.com>
2024-11-26 02:14:38 -0300

    se agrega repetir contraseña

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-import { UserService } from '../../services/user.service';  // Servicio para interactuar con el backend
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -16,20 +16,32 @@ export class RegisterComponent {
     password: '',
     email: '',
     telefono: '',
-    rol: 'Paciente',  // Valor predeterminado, puedes permitir que el rol se seleccione
+    rol: 'Paciente',  // Valor predeterminado
     fecha_nacimiento: ''
   };
+
+  repeatPassword: string = ''; // Nuevo campo para repetir contraseña
 
   constructor(private userService: UserService, private router: Router) {}
 
   onRegister() {
+    if (!this.validatePasswords()) {
+      alert('Las contraseñas no coinciden. Por favor, verifica.');
+      return;
+    }
+
     this.userService.createUser(this.user).subscribe(response => {
       if (response.codigo === 200) {
+        alert('Usuario creado con exito');
         this.router.navigate(['/login']);
       } else {
         console.error('Error al registrar usuario');
       }
     });
+  }
+
+  validatePasswords(): boolean {
+    return this.user.password === this.repeatPassword;
   }
 
   hasRole(): boolean {
